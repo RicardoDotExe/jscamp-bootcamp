000516152a49c8986fef5c83bdfdec5dbe2310d4
madeval <alvarez.fing@gmail.com>
2026-09-02 15:09:07 -0300

    Refactor JobListings.jsx: actualizar importación de JobCard y mejorar mensaje de no resultados

--- 03-react/src/components/jobs/JobListings.jsx
+++ 03-react/src/components/jobs/JobListings.jsx
@@ -1,11 +1,15 @@
-import JobCard from "./JobCard"
+import { JobCard } from "./JobCard"
 
 export function JobListings({ jobs }) {
   return (
     <div>
     <h2>Resultados de búsqueda</h2> 
       {jobs.length === 0 ? (
-        <p>No hay trabajos disponibles.</p>
+        <>
+          {/* <p>No hay trabajos disponibles.</p> */}
+          {/* Tu mensaje está genial :) Hicimos un cambio para que dar un poco más de detalle al usuario. Son opciones, ninguna está mal, si? */}
+          <p>No se han encontrado empleos que coincidan con los criterios de búsqueda</p>
+        </>
       ) : (
         <div className="jobs-listings">
           {jobs.map((job) => (
@@ -17,4 +21,4 @@ export function JobListings({ jobs }) {
   )
 }
 
-export default JobListings
+// export default JobListings
