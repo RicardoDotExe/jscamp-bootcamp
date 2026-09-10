0eb96c83908fe53dc2c394a3fab1f2e5f2111061
madeval <alvarez.fing@gmail.com>
2026-09-10 17:50:49 -0300

    fix: corregir alias de componente Link a RouterLink para evitar confusiones

--- 04-ejercicio-react-router-y-estado-global/src/components/Link.jsx
+++ 04-ejercicio-react-router-y-estado-global/src/components/Link.jsx
@@ -1,10 +1,10 @@
-import { Link  as NavLink} from 'react-router'
+import { Link as RouterLink } from 'react-router'; // Ojo con usar NavLink como alias, ya hay un componente con ese nombre dentro de `react-router` y puede generar confusión
 
 export function Link ({ href, children, ...restOfProps }) {
 
   return (
-    <NavLink to={href} {...restOfProps}>
+    <RouterLink to={href} {...restOfProps}>
       {children}
-    </NavLink>
+    </RouterLink>
   )
 }
